176744389a616648dbbe3494931e9eb49abefd22
hee082024kh <hee082024@gmail.com>
2025-03-02 00:34:39 +0100

    Added field to Item and Inventory

--- character.cs
+++ character.cs
@@ -5,4 +5,7 @@ public class Character
   public required string Name { get; set; }
   public required int Money { get; set; } = 0;
   public List<Item> Inventory { get; set; } = new();
+  public List<Companion> Party { get; set; } = new();
+  public List<Achievement> Achievements { get; set; } = new();
+  public List<Ability> Abilities { get; set; } = new();
 }
--- inventory.cs
+++ inventory.cs
@@ -1,12 +1,17 @@
-public class Item
+public class Item(string name, int bValue, int sValue = 0, int amount = 0, bool exist = false, bool trade = false)
 {
-  public string Name { get; set; }
-  public int Amount { get; set; }
-  public int BuyValue { get; set; }
-  public int SellValue { get; set; }
+  public string Name { get; } = name;
+  public int Amount { get; set; } = amount;
+  public int BuyValue { get; set; } = bValue;
+  public int SellValue { get; set; } = sValue;
+  public bool Exist { get; set; } = exist;
+  public bool Trade { get; } = trade;
 }
 
 public class ItemsAndInventory
 {
-  public List<Item> Inventory { get; set; } = [];
+  public List<Item> Inventory { get; } =
+  [
+    new("Money", 0)
+  ];
 }
