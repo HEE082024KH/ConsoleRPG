03f70515407fbc6667d13c6b1ad860adb6ad87f1
hee082024kh <hee082024@gmail.com>
2025-03-02 02:22:26 +0100

    Added abilities

--- classes/abilities.cs
+++ classes/abilities.cs
@@ -17,29 +17,31 @@ public class AbilitiesList
   [
     // Fire
     new("Fire Spit", "Spits small clusters of fire.", "Fire", 5, 35, 5, 100),
-    new("", "", "Fire", 12, 30, 10, 100),
+    new("Scorch", "A burst of fire.", "Fire", 12, 30, 10, 100),
     new("Fire barrier", "Surrounds the user with a protective barrier of fire.", "Fire", 16, 20, 10, 100),
     new("Fireball", "Hurls a ball of scorching fire.", "Fire", 20, 25, 15, 100),
     new("Cauterize", "Cauterizes wounds using fire.", "Fire", 30, 15, 30, 100),
     new("Searing Bite", "A bite ignited by searing embers.", "Fire", 33, 20, 28, 95),
-    new("Blaze Tornado", "Hurls a tornado of scorching hot fire.", "Fire", 41, 15, 40, 90),
+    new("Flame cannon", "Shoots a volley of fire and lava.", "Fire", 41, 15, 40, 90),
+    new("Fire Blast", "Blasts the target with blazing fire.", "Fire", 46, 10, 45, 100),
     new("Fire Wall", "Conjures a wall of fire protecting the user.", "Fire", 47, 10, 50, 95),
-    new("Molten Eruption", "An eruption of molten lava exploding with velocity.", "Fire", 50, 10, 55, 85),
-    new("", "", "Fire", 57, 0, 0, 0),
-    new("Inferno", "Fire surrounds the user as a blast wave erupts.", "Fire", 65, 5, 80, 80),
-    // Plasma, Flame, Molten, Tornado, Blaze, Heat, Flare, Searing
+    new("Blaze Tornado", "Hurls a tornado of scorching hot fire.", "Fire", 50, 10, 50, 95),
+    new("Molten Eruption", "An eruption of molten lava exploding with velocity.", "Fire", 57, 5, 60, 85),
+    new("Inferno", "Fire surrounds the user as a blast wave erupts.", "Fire", 65, 5, 80, 100),
 
     // Frost
     new("Rime", "", "Frost", 5, 35, 5, 100),
     new("", "", "Frost", 11, 0, 0, 0),
     new("Frostbolt", "Shoots a projectile of frost and cold", "Frost", 15, 20, 15, 100),
-    new("", "", "Frost", 19, 0, 0, 0),
+    new("Chill", "Chills the target, increasing Frost damage taken", "Frost", 19, 20, 2, 95),
     new("", "", "Frost", 29, 0, 0, 0),
     new("", "", "Frost", 32, 0, 0, 0),
     new("", "", "Frost", 39, 0, 0, 0),
     new("", "", "Frost", 45, 0, 0, 0),
     new("", "", "Frost", 51, 0, 0, 0),
     new("", "", "Frost", 58, 0, 0, 0),
-    new("", "", "Frost", 64, 0, 0, 0),
+    new("", "", "Frost", 64, 0, 0, 0)
+
+    // Water, Shadow, Physical, Electric, Ground, Light/Holy
   ];
 }
