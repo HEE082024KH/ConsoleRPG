edc9b6902e8e6c08564251832f205dc4d9a3fe2d
hee082024kh <hee082024@gmail.com>
2025-03-02 01:17:52 +0100

    Added abilities

--- classes/abilities.cs
+++ classes/abilities.cs
@@ -1,10 +1,11 @@
 namespace ConsoleRPG.classes;
 
-public class Ability(string name, string description, string type, int uses, int dmg)
+public class Ability(string name, string description, string type, int levelReq, int uses, int dmg)
 {
   public string Name { get; set; } = name;
   public string Description { get; set; } = description;
   public string Type { get; set; } = type;
+  public int LevelReq { get; set; } = levelReq;
   public int Uses { get; set; } = uses;
   public int Dmg { get; set; } = dmg;
 }
@@ -13,9 +14,9 @@ public class AbilitiesList
 {
   public List<Ability> AbilitiesCompanion =
   [
-  ];
-
-  public List<Ability> AbilitiesCharacter =
-  [
+    new("Burn", "Sets the target ablaze", "Fire", 5, 35, 5),
+    new("Fire spit", "A short distance spray of fire", "Fire", 12, 25, 10),
+    new("Fireball", "A ball projectile of scorching fire", "Fire", 20, 20, 15),
+    new("Frostbolt", "A bolt projectile of frost and cold", "Frost", 13, 20, 15)
   ];
 }
--- classes/character.cs
+++ classes/character.cs
@@ -5,13 +5,11 @@ public class Character(
   int money,
   List<Item> inventory,
   List<Companion> companions,
-  List<Achievement> achievements,
-  List<Ability> abilities)
+  List<Achievement> achievements)
 {
   public required string Name { get; set; }
   public required int Money { get; set; } = 0;
   public List<Item> Inventory { get; set; } = new();
   public List<Companion> Companions { get; set; } = new();
   public List<Achievement> Achievements { get; set; } = new();
-  public List<Ability> AbilitiesCharacter { get; set; } = new();
 }
